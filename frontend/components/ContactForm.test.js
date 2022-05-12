c82c3e42244bae313af197ae23b3156c735e8495
unknown <macrowell@yahoo.com>
2022-05-12 18:19:37 -0400

    completed test 3

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -15,7 +15,11 @@ test('renders the contact form header', () => {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-
+    render(<ContactForm/>);
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    userEvent.type(firstNameInput, "Name");
+    const error = screen.getByText(/error/i)
+    expect(error).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
